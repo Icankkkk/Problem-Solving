0cc4afd3e236300fb7a2fc74e56e3e1bad5910cc
Muhamad Faisal <muhamadfaisal280201@gmail.com>
2021-10-11 21:05:01 +0700

    Update

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    
+    // atribut untuk ObstacleSpawner
+    public GameObject prefabsObstacle;
+    private float y_position = -3.8f; // max posisi untuk y
+    private float x_position = 8; // max posisi untuk x
+
+    // [edit] Membuat List untuk obstacle
+    private List<GameObject> obs = new List<GameObject>();
+    private GameObject obsToSpawn;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnObstacle", 1, 2);
+    }
+
+    void SpawnObstacle()
+    {
+        for (int i = 0; i < obs.Count; i++)
+        {
+            if (obs[i].activeSelf == false)
+            {
+                obs[i].SetActive(true);
+                obs[i].transform.position = PosObs();
+                obsToSpawn = obs[i];
+                return;
+            }
+        }
+
+        if (obsToSpawn == null)
+        {
+            obsToSpawn = Instantiate(prefabsObstacle);
+            obsToSpawn.transform.position = PosObs();
+            obs.Add(obsToSpawn);
+        }
+
+        obsToSpawn = null;
+    }
+
+    Vector3 RandomPos()
+    {
+        float xPos = Random.Range(-x_position, x_position);
+        float yPos = Random.Range(-y_position, y_position);
+
+        return new Vector3(xPos, yPos, 0);
+    }
+
+    Vector3 PosObs()
+    {
+        Vector3 objPos = RandomPos();
+
+        Vector3 playerPos = FindObjectOfType<PlayerMovement>().gameObject.transform.position;
+        while (Vector3.Distance(playerPos, objPos) < 2)
+            objPos = RandomPos();
+
+        return objPos;
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Problem-Solving
+# Problem-Solving
+## Fitur
+Fitur yang ditambahkan pada Problem 8 atau branch ke-8
+* Memunculkan kembali kotak yang sudah tertabrak selama 3 detik setelahnya dengan posisi di acak
